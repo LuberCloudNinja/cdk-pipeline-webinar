2244133079e918ca6b8f693364ca88c6ae384f83
luberguilarte@gmail.com <63034421+LuberCloudNinja@users.noreply.github.com>
2022-04-11 15:35:39 -0700

    Fixing s3_bucket.py file

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from pipelines_webinar.pipelines_webinar_stack import PipelinesWebinarStack
 from pipelines_webinar.pipeline_stack import PipelineStack
 
 app = cdk.App()
-PipelinesWebinarStack(app, "PipelinesWebinarStack")
+# PipelinesWebinarStack(app, "PipelinesWebinarStack")
 
 PipelineStack(app, 'PipelineStack', env={
               'account': "721918345279", 'region': 'us-east-1'})
--- pipelines_webinar/s3_bucket.py
+++ pipelines_webinar/s3_bucket.py
@@ -9,7 +9,7 @@ from constructs import Construct
 
 class S3Bucket(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, env, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         s3.Bucket(self, "Create-cutcket",
